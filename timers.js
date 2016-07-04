fea7a72fb1007d0b875dd77c95c54c431d76b68a
Rencheng Chua <rench00@gmail.com>
2016-07-05 01:37:38 +0800

    comments out the bind

--- timers.js
+++ timers.js
@@ -14,7 +14,8 @@ function stopWatchAndButtons(){
   var initialTime = stopWatch.getSeconds;
 
   var boundInitialTime = initialTime.bind(stopWatch);
-  var startingTime = boundInitialTime();
+  var startingTime = 0
+  // var startingTime = boundInitialTime();
 
   function clickResetBtn(){
     resetBtn.addEventListener('click', function(){
@@ -52,47 +53,6 @@ function stopWatchAndButtons(){
   function pauseTimer(){
     clearInterval(timerResult);
   }
-
-
-  //
-  // var stopWatch = {
-  //   seconds: 0,
-  //   timerId: "timer",
-  //   updateTime: function(seconds){
-  //     this.seconds = setInterval(increaseBySecond, 100);
-  //   }
-  // };
-  //
-  // function increaseBySecond(seconds){
-  //   seconds += 1;
-  // }
-  //
-  // // function increaseBySecond(seconds){
-  // //   console.log(this);
-  // //   this.seconds ++;
-  // // }
-  //
-  // // function timer(seconds, timerId){
-  // //   this.seconds = seconds;
-  // //   this.timerId = timerId;
-  // //   function updateTime(){
-  // //     setInterval(increaseBySecond(seconds), 100);
-  // //   };
-  // //   function increaseBySecond(seconds){
-  // //     seconds += 1;
-  // //   }
-  // // }
-  //
-  // function getUpdatedTime(seconds, callback, callbackObj){
-  //   callback.apply(callbackObj, [seconds]);
-  // }
-  //
-  //
-  // getUpdatedTime(0, stopWatch.updateTime, stopWatch);
-  //
-  // console.log(stopWatch.seconds);
-  //
-  // document.getElementById(stopWatch.timerId).innerText = stopWatch.seconds;
 
 }
 
