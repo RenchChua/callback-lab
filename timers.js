97c99e1985b81ab1bebaabff134769c8dc63049e
Rencheng Chua <rench00@gmail.com>
2016-07-05 18:01:30 +0800

    oop method

--- timers.js
+++ timers.js
@@ -1,59 +1,100 @@
 // your code here:
-function stopWatchAndButtons(){
-  var resetBtn = document.getElementById('reset');
-  var startBtn = document.getElementById('start');
-  var pauseBtn = document.getElementById('pause');
-  var timerResult;
 
-  var stopWatch = {
-    seconds: 0,
-    timerId: "timer",
-    getSeconds: function(){return this.seconds;}
-  };
+var stopwatch = {
+  seconds: 0,
+  timerId: 0,
+  start: document.getElementById('start'),
+  pause: document.getElementById('pause'),
+  reset: document.getElementById('reset'),
+  updateTime: function(){
+    this.seconds ++;
+    timer.innerHTML = "Time elapsed " + this.seconds;
+    console.log(this.seconds);
+  },
 
-  var initialTime = stopWatch.getSeconds;
+  clickStart: function(){
+    // borrowUpdateTime = this.updateTime.bind(this);
+    this.start.addEventListener('click', function(){
+       eventTimerId = setInterval(this.updateTime.bind(this), 1000);
+    }.bind(this));
+  },
 
-  var boundInitialTime = initialTime.bind(stopWatch);
-  var startingTime = 0
-  // var startingTime = boundInitialTime();
+  clickPause: function(){
+    this.pause.addEventListener('click', function(){
+      clearInterval(eventTimerId);
+    }.bind(this));
+  },
 
-  function clickResetBtn(){
-    resetBtn.addEventListener('click', function(){
-      pauseTimer();
-      document.getElementById('timer').innerText = "Stop Watch";
-      console.log(startingTime);
-      startingTime = 0;
-    });
+  clickReset: function(){
+    this.reset.addEventListener('click', function(){
+      clearInterval(eventTimerId);
+      this.seconds = 0;
+      timer.innerHTML = "Stop Watch";
+    }.bind(this));
+  },
+
+  stopwatchFn: function(){
+    this.clickStart();
+    this.clickPause();
+    this.clickReset();
   }
+};
 
-  clickResetBtn();
+stopwatch.stopwatchFn();
 
-  function clickStartbtn(){
-    startBtn.addEventListener('click', updateTime)
-  }
-
-  clickStartbtn();
-
-  function clickPauseBtn(){
-    pauseBtn.addEventListener('click', function(){
-      pauseTimer()
-    });
-  }
-
-  clickPauseBtn();
-
-  function updateTime(startingTime) {
-    timerResult = setInterval(increaseBySecond,1000);
-  }
-
-  function increaseBySecond(){
-    document.getElementById('timer').innerText = startingTime ++
-  }
-
-  function pauseTimer(){
-    clearInterval(timerResult);
-  }
-
-}
-
-stopWatchAndButtons();
+//
+// function stopWatchAndButtons(){
+//
+//   startingTime = stopWatch.getSeconds());
+//
+//   function updateTime(startingTime) {
+//     timerResult = setInterval(increaseBySecond,1000);
+//   }
+//
+//
+//   // var initialTime = stopWatch.getSeconds;
+//   //
+//   // var boundInitialTime = initialTime.bind(stopWatch);
+//   // var startingTime = 0
+//   // // var startingTime = boundInitialTime();
+//   //
+//   // function clickResetBtn(){
+//   //   resetBtn.addEventListener('click', function(){
+//   //     pauseTimer();
+//   //     document.getElementById('timer').innerText = "Stop Watch";
+//   //     console.log(startingTime);
+//   //     startingTime = 0;
+//   //   });
+//   // }
+//   //
+//   // clickResetBtn();
+//   //
+//   // function clickStartbtn(){
+//   //   startBtn.addEventListener('click', updateTime)
+//   // }
+//   //
+//   // clickStartbtn();
+//   //
+//   // function clickPauseBtn(){
+//   //   pauseBtn.addEventListener('click', function(){
+//   //     pauseTimer()
+//   //   });
+//   // }
+//   //
+//   // clickPauseBtn();
+//   //
+//   // function updateTime(startingTime) {
+//   //   timerResult = setInterval(increaseBySecond,1000);
+//   // }
+//   //
+//   // function increaseBySecond(){
+//   //   document.getElementById('timer').innerText = startingTime ++
+//   // }
+//   //
+//   // function pauseTimer(){
+//   //   clearInterval(timerResult);
+//   // }
+//
+// }
+//
+// stopWatchAndButtons();
